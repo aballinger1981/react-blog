82d806faa1a508f11edcec99f0d93897f60ffa8b
Andrew Ballinger <andrew@bricksimple.com>
2020-03-15 22:12:49 -0400

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // SAME AS COMMENTED OUT CODE ABOVE
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
+
+export const fetchPosts = () => {
+    return async (dispatch, getState) => {
+        const response = await jsonPlaceholder.get('/posts');
+
+        dispatch({ type: 'FETCH_POSTS', payload: response.data });
+    };
+};
+
+export const fetchUser = (id) => async (dispatch, getState) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// export const fetchUser = (id) => (dispatch, getState) => _fetchUser(id, dispatch, getState);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
